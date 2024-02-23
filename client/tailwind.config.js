1524830fae56771065682226ffd679bb4103d251
Minh Le <qminh279@gmail.com>
2024-02-23 13:00:13 -0500

    set up tailwind

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "blue-200": "#B1D1F0",
+        "grey-200": "#E9E8E8",
+        "green-300": "#94DA69",
+        "red-100": "#F6EEEE",
+        "red-200": "#E3B3B3",
+      },
+      fontSize: {
+        14: "14px",
+        16: "16px",
+        48: "48px",
+      },
+    },
   },
   plugins: [],
 };
